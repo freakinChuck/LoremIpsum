18c127053ecc99da7f3c218687edb09d939404dc
freakinChuck <silvio.stappung@hotmail.com>
2014-09-25 19:15:04 +0200

    Grundlayout erstellt

---
+++ Source/LoremIpsumWeb/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoremIpsumWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+            //return Content("Hallo");
+        }
+
+        public ActionResult Format()
+        {
+            return View();
+            //return Content("Hallo");
+        }
+
+    }
+}
